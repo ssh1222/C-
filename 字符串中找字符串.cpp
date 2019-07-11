3264527abcf7699a01ad3959011a0edf28a219f3
ssh1222 <2960074137@qq.com>
2019-07-11 21:03:40 +0800

    使用find查找

---
+++ 字符串中找字符串.cpp
@@ -0,0 +1,19 @@
+////查找输入的字符串中指定的字符串，并输出其首字符的位置；
+//#include <iostream>
+//#include <string>
+//#include <stdlib.h>
+//
+//int main(){
+//	std::string str1, str2;
+//	std::cout << "str1:" << std::endl;
+//	getline(std::cin, str1);
+//	std::cout << "str2:" << std::endl;
+//	getline(std::cin, str2);
+//	size_t pos = str1.find(str2);
+//	if (pos == std::string::npos)
+//		std::cout << "not find" << std::endl;
+//	else
+//		std::cout << pos << std::endl;
+//	system("pause");
+//	return 0;
+//}
