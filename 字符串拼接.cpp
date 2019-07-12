9d7f7d4493d684a75b9be73b6c968ac344b8e563
ssh1222 <2960074137@qq.com>
2019-07-12 22:01:06 +0800

    直接使用+=接口

---
+++ 字符串拼接.cpp
@@ -0,0 +1,19 @@
+////将多个字符拼接，每两个字符串间以：间隔；
+//#include <iostream>
+//#include <string>
+//#include <stdlib.h>
+//
+//int main(){
+//	int num;
+//	std::string str;
+//	std::cout << "输入字符串个数：";
+//	std::cin >> num;  
+//	while (num--){
+//		std::string str_tmp;
+//		std::cin >> str_tmp;
+//		str += str_tmp;
+//	}
+//	std::cout << str << std::endl;
+//	system("pause");
+//	return 0;
+//}
