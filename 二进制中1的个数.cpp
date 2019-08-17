01f9b9185c29f32564cc1b1f9b2baea40325fed4
ssh1222 <2960074137@qq.com>
2019-08-17 23:33:25 +0800

    一个数左移与整数相遇

---
+++ 二进制中1的个数.cpp
@@ -0,0 +1,17 @@
+//题目描述
+//输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+
+class Solution {
+public:
+	int  NumberOf1(int n)
+	{
+		int count = 0;
+		unsigned int flag = 1;
+		while (flag){
+			if (n & flag)
+				++count;
+			flag = flag << 1;
+		}
+		return count;
+	}
+};
